0f3b8858edc12745b52fcd2768b506346788b668
rizkygm23 <rizky230504@gmail.com>
2024-07-28 17:47:54 +0700

    final 1.0

--- src/script.js
+++ src/script.js
@@ -61,70 +61,80 @@ const cardData = [
         title: "InvestaSync",
         description: "InvestaSync is an application used to monitor all investment portfolios from various sectors by connecting APIs from the brokers or exchanges you use. With InvestaSync, you can manage and track the performance of your investments in real-time on a single, user-friendly platform.",
         imageUrl: "./img/ui01.png",
-        buttonImageUrl: "./img/next.png"
+        buttonImageUrl: "./img/next.png",
+        moreUrl:'https://www.behance.net/gallery/189571381/Investa-Aggregator-for-Investing-in-Stocks-Crypto'
       },
       {
         type: "uiux",
         title: "Zkrow",
         description: "Zkrow is a borrowing and lending project in the crypto world. With Zkrow, users can borrow crypto assets with collateral or offer their assets to be lent out and earn interest. The platform utilizes the latest technology to ensure security and efficiency in every transaction, making it a reliable and transparent solution for users looking to optimize their crypto assets.",
         imageUrl: "./img/ui02.png",
-        buttonImageUrl: "./img/next.png"
+        buttonImageUrl: "./img/next.png",
+        moreUrl:"https://www.behance.net/gallery/189569487/Web3-Landing-page-ZkRow"
       },
       {
         type: "uiux",
         title: "Drop Wallet",
         description: "Drop Wallet is a crypto wallet with a unique feature that allows users to monitor and provide statistics on their wallet, especially those based on EVM (Ethereum Virtual Machine). In addition to basic wallet functions, Drop Wallet offers in-depth analysis and statistical reports on your crypto asset activities and values.",
         imageUrl: "./img/ui03.png",
-        buttonImageUrl: "./img/next.png"
+        buttonImageUrl: "./img/next.png",
+        moreUrl:"https://www.behance.net/gallery/175953233/Retdrop-Wallet"
       },
       {
         type: "uiux",
         title: "Hinkal Protocol",
         description: "Hinkal is a project that uses Account Abstraction (AA) to enhance security and ease of use in blockchain transactions. By leveraging AA technology, Hinkal allows users to have more flexible control over their accounts, including private key management and transaction permissions.",
         imageUrl: "./img/ui04.png",
-        buttonImageUrl: "./img/next.png"
+        buttonImageUrl: "./img/next.png",
+        moreUrl:"https://hinkal.pro/"
       },
       {
         type: "uiux",
         title: "NFT Marketplace",
         description: "NFT Marketplace is a digital platform that enables users to buy, sell, and trade Non-Fungible Tokens (NFTs). The marketplace offers various features such as auctions, direct offers, and integration with multiple blockchains.",
         imageUrl: "./img/ui05.png",
-        buttonImageUrl: "./img/next.png"
+        buttonImageUrl: "./img/next.png",
+        moreUrl:"https://dribbble.com/shots/20891660-Hyped-Market-a-NFT-MARKETPLACE"
       },
     {
       type: "node",
       title: "Avail",
       description: "Avail is a Web3 infrastructure layer that allows modular execution layers to scale and interoperate in a trust minimized way.",
       imageUrl: "./img/Avail.png",
-      buttonImageUrl: "./img/next.png"
+      buttonImageUrl: "./img/next.png",
+      moreUrl:"https://www.availproject.org/"
     },
     {
       type: "node",
       title: "Aleo",
       description: "True ZK should provide all three. Aleo is the first place that brings all the power of ZK under one roof.",
       imageUrl: "./img/aleo.png",
-      buttonImageUrl: "./img/next.png"
+      buttonImageUrl: "./img/next.png",
+      moreUrl:"https://aleo.org/"
     },
     {
       type: "node",
       title: "Bevm",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, nisi non providens aperiam quidem fugit dolorem dignissimos sapiente ullam, eius hic totam a porro laboriosam autem.",
+      description: "BEVM is the first fully decentralized EVM-compatible Bitcoin L2 that uses BTC as Gas. It allows all DApps which can run in the Ethereum ecosystem to operate on Bitcoin L2",
       imageUrl: "./img/bevm.png",
-      buttonImageUrl: "./img/next.png"
+      buttonImageUrl: "./img/next.png",
+      moreUrl:"https://www.bevm.io/"
     },
     {
       type: "node",
       title: "Rivalz",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, nisi non providens aperiam quidem fugit dolorem dignissimos sapiente ullam, eius hic totam a porro laboriosam autem.",
+      description: "Use Rivalz for decentralized data processing and storageAccess networks of Trusted Data and Intel providers.",
       imageUrl: "./img/rivalz.png",
-      buttonImageUrl: "./img/next.png"
+      buttonImageUrl: "./img/next.png",
+      moreUrl:"https://rivalz.ai/"
     },
     {
       type: "node",
       title: "Sonaric",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, nisi non providens aperiam quidem fugit dolorem dignissimos sapiente ullam, eius hic totam a porro laboriosam autem.",
+      description: "The first AI-powered network that optimizes the allocation and management of all compute across all chains to perfectly reward all contributors.",
       imageUrl: "./img/sonaric.png",
-      buttonImageUrl: "./img/next.png"
+      buttonImageUrl: "./img/next.png",
+      moreUrl:"https://sonaric.xyz/"
     }
   ];
   
@@ -145,9 +155,11 @@ const cardData = [
         <div class="p-6">
           <div class="grid grid-cols-2">
             <h1 class="text-2xl font-sans font-semibold text-slate-600 mt-auto mb-auto">${card.title}</h1>
-            <button class="bg-slate-300/60 rounded-full w-16 h-16 ml-auto hover:bg-slate-200 hover:-rotate-45 hover:transition-transform ease-in-out">
-              <img class="max-w-6 mx-auto" src="${card.buttonImageUrl}" alt="">
-            </button>
+            <a class=" ml-auto " href="${card.moreUrl}" target="_blank" rel="noopener noreferrer">
+              <button class="bg-slate-300/60 rounded-full w-16 h-16 ml-auto hover:bg-slate-200 hover:-rotate-45 hover:transition-transform ease-in-out">
+                <img class="max-w-6 mx-auto" src="${card.buttonImageUrl}" alt="">
+              </button>
+            </a>
           </div>
           <h2 class="mt-3 line-clamp-3 min-h-line font-sans font-medium text-slate-500 text-justify text-xl">${card.description}</h2>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+const { transform } = require('lodash');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content:["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        'border-spin': {
+          '100%': {
+            transform: 'rotate(-360deg)'
+          },
+        },
+        'border-spins': {
+          '100%': {
+            transform: 'rotate(-360deg)'
+          },
+        },
+      }
+
+    },
+    animation: {
+      'border-spin': 'border-spin 3s linear infinite',
+      'border-spins': 'border-spin 1s linear infinite'
+    }
+  
   },
   plugins: [],
 }
